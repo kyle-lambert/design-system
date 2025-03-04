aea91405cfa5a647a6347434a093216a29f8fa45
Kyle Lambert <49013078+kyle-lambert@users.noreply.github.com>
2025-03-04 21:15:06 +1030

    Input icons

--- app/components/input.tsx
+++ app/components/input.tsx
@@ -1,47 +1,86 @@
 import { ark, type HTMLArkProps } from '@ark-ui/react';
-import React from 'react';
+import React, { type ReactNode } from 'react';
 import { cn, tva, type VariantProps } from '~/utils';
 
 const inputStyles = tva({
-  base: 'rounded-lg text-sm text-gray-950 inset-ring shadow-xs inset-ring-gray-200 transition-[color_background-color] outline-none placeholder:text-gray-400 focus:inset-ring-[1.5px] focus:inset-ring-blue-600 disabled:pointer-events-none disabled:opacity-50 aria-invalid:inset-ring-[1.5px] aria-invalid:inset-ring-red-600 aria-invalid:focus:inset-ring-red-600',
+  slots: {
+    root: 'relative cursor-text',
+    icon: 'absolute top-0 bottom-0 z-10 inline-flex w-9 shrink-0 items-center justify-center text-neutral-600 transition-[color_background-color] peer-hover:text-neutral-800 peer-focus:text-indigo-700 peer-disabled:opacity-50 peer-aria-invalid:text-red-700 peer-aria-invalid:peer-focus:text-red-700 [&_svg]:size-[1.2em] [&_svg]:shrink-0',
+    input:
+      'peer w-full rounded-lg bg-white text-neutral-950 inset-ring shadow-sm inset-ring-neutral-300 transition-[color_background-color] outline-none placeholder:text-neutral-400 hover:inset-ring-neutral-400 focus:inset-ring-[1.5px] focus:ring-3 focus:ring-indigo-600/15 focus:inset-ring-indigo-600 disabled:pointer-events-none disabled:opacity-50 aria-invalid:inset-ring-[1.5px] aria-invalid:inset-ring-red-600 aria-invalid:focus:ring-3 aria-invalid:focus:ring-red-600/15 aria-invalid:focus:inset-ring-red-600',
+  },
   variants: {
-    icon: {
+    size: {
+      sm: {
+        input: 'h-10 px-3 text-sm',
+      },
+      default: {
+        input: 'h-12 px-3 text-sm',
+      },
+      lg: {
+        input: 'h-14 px-3 text-base',
+      },
+    },
+    iconPosition: {
       start: '',
       end: '',
-    },
-    size: {
-      sm: 'h-10 px-3',
-      lg: 'h-12 px-3',
     },
   },
   compoundVariants: [
     {
-      icon: 'start',
-      size: ['sm', 'lg'],
-      class: 'pr-3 pl-[calc(var(--spacing(3))+var(--spacing(4)))]',
+      iconPosition: 'start',
+      size: ['sm', 'default', 'lg'],
+      class: {
+        icon: 'left-0 w-9',
+        input: 'pr-3 pl-[calc(theme(spacing.9))]',
+      },
     },
     {
-      icon: 'end',
-      size: ['sm', 'lg'],
-      class: 'pr-[calc(var(--spacing(3))+var(--spacing(4)))] pl-3',
+      iconPosition: 'end',
+      size: ['sm', 'default', 'lg'],
+      class: {
+        icon: 'right-0 w-9',
+        input: 'pr-[calc(theme(spacing.3)+theme(spacing.6))] pl-3',
+      },
     },
   ],
   defaultVariants: {
-    size: 'sm',
+    size: 'default',
   },
 });
 
+type DiscriminatedIcon =
+  | {
+      iconStart?: ReactNode;
+      iconEnd?: never;
+    }
+  | {
+      iconStart?: never;
+      iconEnd?: ReactNode;
+    };
+
 export type InputProps = Omit<HTMLArkProps<'input'>, 'size'> &
-  VariantProps<typeof inputStyles>;
+  Omit<VariantProps<typeof inputStyles>, 'iconPosition'> &
+  DiscriminatedIcon;
 
 export const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, icon, size, ...rest }, ref) => {
+  ({ className, iconStart, iconEnd, size, ...rest }, ref) => {
+    const iconPosition = iconStart ? 'start' : iconEnd ? 'end' : undefined;
+    const styleSlots = inputStyles({ size, iconPosition });
     return (
-      <ark.input
-        ref={ref}
-        className={cn(inputStyles({ icon, size }), className)}
-        {...rest}
-      />
+      <label className={styleSlots.root()}>
+        <ark.input
+          ref={ref}
+          className={cn(styleSlots.input(), className)}
+          {...rest}
+        />
+        {iconPosition === 'start' ? (
+          <div className={styleSlots.icon()}>{iconStart}</div>
+        ) : null}
+        {iconPosition === 'end' ? (
+          <div className={styleSlots.icon()}>{iconEnd}</div>
+        ) : null}
+      </label>
     );
   },
 );
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,9 +3,22 @@ import { Input } from '~/components/input';
 
 export default function Home(args: Route.ComponentProps) {
   return (
-    <div className="grid grid-cols-5 justify-center gap-4 p-10">
-      <Input size="sm" placeholder="Small" />
-      <Input size="sm" aria-invalid="true" placeholder="Small" />
+    <div className="grid gap-4 p-10">
+      <div className="grid grid-cols-4 gap-4">
+        <Input size="sm" placeholder="Small" />
+        <Input size="sm" placeholder="Disabled" disabled />
+        <Input size="sm" placeholder="Error" aria-invalid />
+      </div>
+      <div className="grid grid-cols-4 gap-4">
+        <Input size="default" placeholder="Small" />
+        <Input size="default" placeholder="Disabled" disabled />
+        <Input size="default" placeholder="Error" aria-invalid />
+      </div>
+      <div className="grid grid-cols-4 gap-4">
+        <Input size="lg" placeholder="Small" />
+        <Input size="lg" placeholder="Disabled" disabled />
+        <Input size="lg" placeholder="Error" aria-invalid />
+      </div>
     </div>
   );
 }
